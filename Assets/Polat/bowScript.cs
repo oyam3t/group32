3a6ffa74b74fce5bc2dde6f94e13763efeee365b
Polatcan Gürbüz <427638@ogr.ktu.edu.tr>
2024-05-18 21:22:27 +0300

    ilkgüncommitim
    ok fırlatma, zamanı f tuşu ile yavaşlatma, zamanı yavaşlatıp 0.8 saniye bekletme, skilli yeniden kullanabilmek için 2 saniye bekletme, atıldığında oku destroy etme, mouse ile oku hareket ettirme görevleri yapıldı

---
+++ Assets/Polat/bowScript.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bowScript : MonoBehaviour
+{
+    [SerializeField]
+    float time = 0.25f;
+    [SerializeField]
+    float addscore = 10f;
+    [SerializeField]
+    Rigidbody2D rb2d;
+    Vector2 difference = Vector2.zero;
+    bool finished = true;
+    [SerializeField]
+    private GameObject bullet;
+    
+    private void Start()
+    {
+        rb2d = bullet.gameObject.GetComponent<Rigidbody2D>();    
+    }
+    void Update()
+    {
+        
+        if (Input.GetKey(KeyCode.F) && finished == true)
+        {
+            Time.timeScale = time; // zaman yavaþlatma
+            StartCoroutine(skillclock());
+            finished = false;
+            Debug.Log("ilk saniye");
+            StartCoroutine(skillwaitclock());
+        }
+        
+    }//update
+    private void FixedUpdate()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            try {
+                rb2d.AddForce(new Vector2(addscore, 0.5f));// oku hýzlandýrma
+            } catch {
+            
+            }
+            
+        }
+    }//fixed update
+    private void OnMouseDown()
+    {
+        difference = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition) - (Vector2)transform.position;
+    }//on mouse down
+
+    private void OnMouseDrag()
+    {
+        transform.position = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition) - difference;
+    }// on mouse drag
+    IEnumerator skillclock()// zamaný 0.8 saniye sonra tekrar kendi haline döndürme
+    {
+        yield return new WaitForSeconds(0.8f);
+        Time.timeScale = 1.0f;
+    }
+    IEnumerator skillwaitclock()//skill kullanabilme için bekletme
+    {
+        Debug.Log("2 saniye skilli yeniden kullanabilme için beklemeye alýndý");
+        yield return new WaitForSeconds(2f);
+        finished = true;
+        Debug.Log("son saniye");
+    }
+
+}//class
